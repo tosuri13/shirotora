d1903422e89a23e44731a9a5ba33d0a363221c19
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-03-13 20:35:34 +0900

    :hammer: 初めてのcomponentsを作成

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(props) {
+  return (
+    <div>
+      <h1>これ...ヘッダーっす!!</h1>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from "next";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const IndexPage: NextPage = () => {
-  return <div>しろとら建設予定地</div>;
+  return (
+    <div>
+      <Header />
+      <div>しろとら建設予定地</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default IndexPage;
