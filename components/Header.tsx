327cb2e24403aaa8b7280ad8d415039b0d2e3df2
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-03-14 00:01:44 +0900

    :hammer: ヘッダーを作成(CSSも若干書いた)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
+import styles from "../styles/Header.module.css";
+
 export default function Header(props) {
   return (
-    <div>
-      <h1>これ...ヘッダーっす!!</h1>
+    <div className={styles.header}>
+      <h1>SHIROTORA(仮)</h1>
+      <div className={styles.menu}>
+        <p>Home</p>
+        <p>About</p>
+      </div>
     </div>
   );
 }
