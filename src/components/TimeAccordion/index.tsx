5572011ea2d3733a8987458cbeb086cd88ea678f
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2025-05-13 10:45:46 +0900

    :arrow_up: Lucideとdate-fnsを追加

--- src/components/TimeAccordion/index.tsx
+++ src/components/TimeAccordion/index.tsx
@@ -6,6 +6,7 @@ import {
   Root as AccordionRoot,
   AccordionTrigger,
 } from "@radix-ui/react-accordion";
+import { CircleChevronDown } from "lucide-react";
 
 export type TimeAccordionProps = {
   timeTable: { [key: string]: Record[] };
@@ -16,8 +17,9 @@ export const TimeAccordion = ({ timeTable }: TimeAccordionProps) => {
     <AccordionRoot className="flex w-full flex-col gap-[8px]" type="multiple">
       {Object.entries(timeTable).map(([baseTime, records]) => (
         <AccordionItem key={baseTime} value={baseTime}>
-          <AccordionTrigger className="h-[32px] w-full rounded-[8px] bg-theme-primary px-[12px] text-left font-semibold text-[16px] text-white">
-            {baseTime}
+          <AccordionTrigger className="flex h-[32px] w-full items-center gap-[4px] rounded-[8px] bg-theme-primary px-[12px]">
+            <CircleChevronDown className="size-[16px] stroke-white" />
+            <p className="font-semibold text-[16px] text-white">{baseTime}</p>
           </AccordionTrigger>
           <AccordionContent className="flex w-full flex-col gap-[8px] data-[state=open]:mt-[8px]">
             {records.map((record) => (
