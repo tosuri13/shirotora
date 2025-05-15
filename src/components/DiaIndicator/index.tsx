3aa599f8f820e7a0eaa8d458b127ed5dafc24201
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2025-05-15 21:23:40 +0900

    :lipstick: DiaIndicatorのローディングを修正

--- src/components/DiaIndicator/index.tsx
+++ src/components/DiaIndicator/index.tsx
@@ -1,32 +1,23 @@
 "use client";
 
 import { SubwayIcon } from "@/components/icons";
-import { LoaderCircle } from "lucide-react";
 import { useDiaIndicator } from "./hooks";
 
 export const DiaIndicator = () => {
   const { isHoliday } = useDiaIndicator();
 
-  const Indicator = () => {
-    if (isHoliday === undefined) {
-      return (
-        <LoaderCircle className="size-[20px] animate-spin stroke-theme-primary" />
-      );
-    }
-
+  if (isHoliday === undefined) {
     return (
-      <>
-        <SubwayIcon className="h-[24px] w-[24px]" />
-        <p className="font-semibold text-[16px] text-black">
-          {`${isHoliday ? "休日" : "平日"}ダイヤ`}
-        </p>
-      </>
+      <div className="h-[32px] w-[120px] animate-pulse rounded-[4px] bg-theme-primary/20" />
     );
-  };
+  }
 
   return (
     <div className="flex w-fit items-center justify-between gap-[4px] border-theme-primary border-b p-[4px]">
-      <Indicator />
+      <SubwayIcon className="h-[24px] w-[24px]" />
+      <p className="font-semibold text-[16px] text-black">
+        {`${isHoliday ? "休日" : "平日"}ダイヤ`}
+      </p>
     </div>
   );
 };
